c3e7e57436cb1bc963a34defbdd5c6e94e0e3728
Federico Angelo Mor <federangelomor@gmail.com>
2026-02-06 14:58:03 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,23 +6,24 @@ DocMeta.setdocmeta!(Enigmistics, :DocTestSetup, :(using Enigmistics); recursive=
 
 makedocs(
     sitename = "Enigmistics.jl",
+    repo="https://github.com/federicomor/Enigmistics.jl",
     format = Documenter.HTML(
-        repolink = "https://github.com/federicomor/Enigmistics"
+        prettyurls=false
+        # repolink = "https://github.com/federicomor/Enigmistics"
         # repolink = "...", # if testing locally?
         # inventory_version = "0.1.0" # if testing locally?
     ),  
     modules = [Enigmistics], doctest = true, # run doctests
     checkdocs=:exports, # check for docstrings only on exported functions
     pages = [
-        "index.md",
-        "wordgames.md",
-        "crosswords.md"
+        "Home" =>"index.md",
+        "Wordgames" => "wordgames.md",
+        "Crosswords" => "crosswords.md"
         ]
     # remotes = nothing # if testing locally?
 )
 
 deploydocs(
     repo = "github.com/federicomor/Enigmistics.jl",
-    branch = "main",
-    versions = nothing
+    devbranch = "main"
 )
